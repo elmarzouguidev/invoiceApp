cddeaf66db615f0656685dd9521e07773c34e6da
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2023-05-04 19:17:32 +0100

    clean up v2

--- routes/admin/login.php
+++ routes/admin/login.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', [LoginController::class, 'loginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->middleware('throttle:login')->name('loginPost');
 
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
--- routes/admin/routes.php
+++ routes/admin/routes.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
