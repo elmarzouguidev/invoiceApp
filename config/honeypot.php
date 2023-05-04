6d8c08c758f2e70d3161917bfa4542a629ea59c8
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2023-05-04 16:36:54 +0100

    add spatie's packages

--- config/honeypot.php
+++ config/honeypot.php
@@ -13,7 +13,7 @@ return [
      * value for this name will be discarded. Make sure this name does not
      * collide with a form field that is actually used.
      */
-    'name_field_name' => env('HONEYPOT_NAME', 'my_name'),
+    'name_field_name' => env('HONEYPOT_NAME', 'invoiceApp'),
 
     /*
      * When this is activated there will be a random string added
@@ -33,7 +33,7 @@ return [
      * if the form wasn't submitted too quickly. Make sure this name does not
      * collide with a form field that is actually used.
      */
-    'valid_from_field_name' => env('HONEYPOT_VALID_FROM', 'valid_from'),
+    'valid_from_field_name' => env('HONEYPOT_VALID_FROM', 'valid_from_app'),
 
     /*
      * If the form is submitted faster than this amount of seconds
