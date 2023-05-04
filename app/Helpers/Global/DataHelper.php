8cf9b1be41a15a52732a6ad7b886f57511c5c442
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2023-05-04 16:59:55 +0100

    add a philosophy to autoload my helper functions from a Service Provider

---
+++ app/Helpers/Global/DataHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!function_exists('appData')) {
+    /**
+     * Helper to grab the application name.
+     *
+     * @return mixed
+     */
+    function appData()
+    {
+        return "yes abdo data";
+    }
+}
